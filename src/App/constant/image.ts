513983258e6e0d609bf79069b7c4adec399b48f0
easy-zero <easy0.dev@gmail.com>
2022-06-12 16:15:12 +0900

    refactor: Image constant 추가  - assets/images 구조 변경  - image export

---
+++ src/App/constant/image.ts
@@ -0,0 +1,9 @@
+const Image = {
+  directory: require("../assets/images/directory.png"),
+  file: require("../assets/images/file.png"),
+  loading: require("../assets/images/nyan-cat.gif"),
+  prev: require("../assets/images/prev.png"),
+  sample_image: require("../assets/images/sample_image.jpeg"),
+};
+
+export default Image;
